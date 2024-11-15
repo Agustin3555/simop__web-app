afd693337ae19809ca6497343ce138a97e23bd15
Agustin3555 <Agustin3555@hotmail.com>
2024-11-15 18:05:22 -0300

    feat:L Limpia el proyecto.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import './styles/index.css'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
