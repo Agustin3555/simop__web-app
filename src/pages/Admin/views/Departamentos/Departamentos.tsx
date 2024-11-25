47138ccf1ba47653d5f0fbdda7e879fcc67d543e
Agustin3555 <Agustin3555@hotmail.com>
2024-11-25 01:30:03 -0300

    feat(direccion, departamento)

--- src/pages/Admin/views/Departamentos/Departamentos.tsx
+++ src/pages/Admin/views/Departamentos/Departamentos.tsx
@@ -1,9 +1,9 @@
 import { View } from '../../components'
-import { Add } from './components'
+import { Add, Query } from './components'
 import { ViewKey } from '../../enums'
 
 const Departamentos = () => {
-  return <View view={ViewKey.DEPARTAMENTOS} add={<Add />} />
+  return <View view={ViewKey.DEPARTAMENTOS} add={<Add />} query={<Query />} />
 }
 
 export default Departamentos
--- src/pages/Admin/views/SubSecretarias/SubSecretarias.tsx
+++ src/pages/Admin/views/SubSecretarias/SubSecretarias.tsx
@@ -1,9 +1,9 @@
 import { View } from '../../components'
-import { Add } from './components'
+import { Add, Query } from './components'
 import { ViewKey } from '../../enums'
 
 const SubSecretarias = () => {
-  return <View view={ViewKey.SUB_SECRETARIAS} add={<Add />} />
+  return <View view={ViewKey.SUB_SECRETARIAS} add={<Add />} query={<Query />} />
 }
 
 export default SubSecretarias
