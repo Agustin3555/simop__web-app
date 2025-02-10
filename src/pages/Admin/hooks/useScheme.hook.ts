b7b123a61e13a27a9f59c9934a0edc75a8bbbb5c
Agustin3555 <Agustin3555@hotmail.com>
2025-02-10 17:17:41 -0300

    feat(useScheme): Optimiza

--- src/pages/Admin/hooks/useScheme.hook.ts
+++ src/pages/Admin/hooks/useScheme.hook.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { SchemeContext } from '../contexts'
 import { EntityKey } from '@/services/config'
 import { PropScheme } from '../services/config'
@@ -6,11 +6,15 @@ import { PropScheme } from '../services/config'
 export const useScheme = () => {
   const { scheme } = useContext(SchemeContext)!
 
-  const flatProps = scheme.groups.reduce((acc, { props }) => {
-    Object.values(props).forEach(prop => (acc[prop.key] = prop))
+  const flatProps = useMemo(
+    () =>
+      scheme.groups.reduce((acc, { props }) => {
+        Object.values(props).forEach(prop => (acc[prop.key] = prop))
 
-    return acc
-  }, {} as Record<EntityKey, PropScheme>)
+        return acc
+      }, {} as Record<EntityKey, PropScheme>),
+    [],
+  )
 
   return { scheme, flatProps }
 }
