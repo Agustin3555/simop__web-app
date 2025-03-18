2c683a8d2411772a45ff8ad75772a978108bf41b
Agustin3555 <Agustin3555@hotmail.com>
2025-03-18 08:34:16 -0300

    feat: Crea la navegación para el tablero

--- src/pages/Admin/constants/navTree.const.ts
+++ src/pages/Admin/constants/navTree.const.ts
@@ -114,4 +114,8 @@ export const TREE: SectionNode[] = [
       { scheme: RecepcionModel.scheme },
     ],
   },
+  {
+    title: 'Tablero de Control',
+    sections: [{ scheme: ObraModel.scheme }],
+  },
 ]
