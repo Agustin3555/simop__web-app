7f17bdf42699aa47e60badd1761359126c5c95c7
Agustin3555 <Agustin3555@hotmail.com>
2025-06-19 11:02:02 -0300

    feat(DataDownloadBanner)

--- src/pages/Admin/components/DataDownloadBanner/DataDownloadBanner.tsx
+++ src/pages/Admin/components/DataDownloadBanner/DataDownloadBanner.tsx
@@ -3,15 +3,14 @@ import { Icon, Loader } from '@/components'
 import { ActionState } from '@/hooks'
 import { classList, varList } from '@/helpers'
 import { useMemo } from 'react'
-import { Size } from '@/styles/palette'
 
-const SIZES = [Size.XS2, Size.S, Size.M]
+const SIZES = ['s', 's', 's', 'm', 'm', 'l']
 
 const generateParticles = (count: number) =>
   Array.from({ length: count }, () => ({
     x: `${Math.random() * 100}%`,
     y: `${Math.random() * 100}%`,
-    size: SIZES[Math.floor(Math.random() * SIZES.length)] + 'rem',
+    size: SIZES[Math.floor(Math.random() * SIZES.length)],
   }))
 
 export interface DataDownloadBannerProps {
@@ -23,7 +22,7 @@ const DataDownloadBanner = ({
   actionState,
   onDownload,
 }: DataDownloadBannerProps) => {
-  const particles = useMemo(() => generateParticles(20), [])
+  const particles = useMemo(() => generateParticles(16), [])
 
   return (
     <button
@@ -34,11 +33,11 @@ const DataDownloadBanner = ({
       disabled={actionState !== 'ready'}
       onClick={onDownload}
     >
-      <div className="particles">
-        {particles.map((vars, i) => (
-          <div key={i} style={varList({ ...vars })} />
-        ))}
-      </div>
+      {particles.map(({ x, y, size }, i) => (
+        <div key={i} className={classList('particle-container', size)}>
+          <div style={varList({ x, y })} />
+        </div>
+      ))}
       <div className="catalyst">
         <Icon
           faIcon="fa-solid fa-cloud-arrow-down"
